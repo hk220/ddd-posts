715f2ea534f2edc4655f33411a428379cf5c7a6a
Kazuki Hara <yukidama.k@gmail.com>
2020-08-08 14:04:29 +0000

    injectorの使い方を修正する

--- tests/application/test_post.py
+++ tests/application/test_post.py
@@ -3,7 +3,6 @@ import string
 import pytest
 from injector import Injector
 
-from ddd_posts.domain.model.post import PostFactoryInterface, PostRepositoryInterface
 from ddd_posts.in_memory_infrastructure.post import InMemoryPostModule
 from ddd_posts.application.post import PostApplicationService
 
@@ -11,9 +10,7 @@ from ddd_posts.application.post import PostApplicationService
 @pytest.fixture
 def service():
     injector = Injector([InMemoryPostModule])
-    factory: PostFactoryInterface = injector.get(PostFactoryInterface)
-    repository: PostRepositoryInterface = injector.get(PostRepositoryInterface)
-    return PostApplicationService(factory, repository)
+    return injector.get(PostApplicationService)
 
 
 def test_one_post(service):
