03ddb81236c8be526474b876f581c946e54c82f2
Kazuki Hara <yukidama.k@gmail.com>
2020-08-09 00:53:52 +0000

    Fix application test

--- tests/application/test_post.py
+++ tests/application/test_post.py
@@ -3,8 +3,8 @@ import string
 import pytest
 from injector import Injector
 
-from ddd_posts.in_memory_infrastructure.post import InMemoryPostModule
 from ddd_posts.application.post import PostApplicationService
+from ddd_posts.in_memory_infrastructure.post import InMemoryPostModule
 
 
 @pytest.fixture
@@ -14,8 +14,12 @@ def service():
 
 
 def test_one_post(service):
-    post_id = service.post(string.ascii_letters)
-    result = service.detail(post_id)
-    assert post_id == result.post_id
-    assert string.ascii_letters == result.post_content.value
-    assert len(service.list_()) == 1
+    post_id1 = service.post(string.ascii_uppercase)
+    post_id2 = service.post(string.ascii_lowercase)
+    result1 = service.detail(post_id1)
+    result2 = service.detail(post_id2)
+    assert post_id1 == result1.post_id
+    assert post_id2 == result2.post_id
+    assert string.ascii_uppercase == result1.post_content.value
+    assert string.ascii_lowercase == result2.post_content.value
+    assert len(service.list_()) == 2
