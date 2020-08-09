fe1757290ab6572114964c0b3d03dada75731de6
Kazuki Hara <yukidama.k@gmail.com>
2020-08-09 01:57:52 +0000

    dataclassにinject.autoparamsするように変更する

--- ddd_posts/application/post/__init__.py
+++ ddd_posts/application/post/__init__.py
@@ -7,19 +7,13 @@ from ddd_posts.domain.model.post import PostFactoryInterface, PostRepositoryInte
 from ddd_posts.domain.model.post import PostID, Post
 
 
+@inject.autoparams()
 @dataclass
 class PostApplicationService:
     factory: PostFactoryInterface
     repository: PostRepositoryInterface
 
-    @inject.autoparams()
-    def __init__(self, factory: PostFactoryInterface, repository: PostRepositoryInterface):
-        self.factory = factory
-        self.repository = repository
-
     def post(self, content: str) -> PostID:
-        if not isinstance(content, str):
-            raise ValueError("contentがないよ")
         post: Post = self.factory.create(content)
         self.repository.save(post)
         return post.post_id
